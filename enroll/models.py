adbaf707cdda6238c10374766ca31a70b0ddfcb8
Kazi Mushfiqur Rahman <52573809+mushfiq1998@users.noreply.github.com>
2023-05-04 14:21:14 +0600

    Add files via upload

---
+++ enroll/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    id = models.IntegerField(primary_key='True')
+    name = models.CharField(max_length=70)
+    email = models.EmailField(max_length=70)
+    password = models.CharField(max_length=70)
+    comment = models.CharField(max_length=70, default='Not available')
